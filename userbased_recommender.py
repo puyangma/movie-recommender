f243ac4a39f937acf2590b63ee60f917d6e77ca0
puyangma <ma.puyang@gmail.com>
2017-05-17 22:41:27 -0400

    Update userbased_recommender.py

--- userbased_recommender.py
+++ userbased_recommender.py
@@ -22,3 +22,23 @@ def recommender(dict, user, distance_metric, n):
     result.sort(key=lambda tup:tup[0],reverse=True)
     top=result[:n]
     return top
+
+def recommender(dict, user, distance_metric):
+    #recommend items for the user in the format of (predicted rating, item_name)
+    weighted_rating={}
+    weights={}
+    threshold=0.2
+    for neib in dict:
+        if(neib != user):
+            dist=distance_metric(dict,user,neib)
+            for mv in dict[neib]:
+                if(mv not in dict[user] and dist>threshold):
+                    if(mv not in weighted_rating):
+                        weighted_rating.setdefault(mv,0)
+                        weights.setdefault(mv,0)
+                        
+                    weighted_rating[mv] += dist*dict[neib][mv]
+                    weights[mv] += dist
+    result = [(weighted_rating[i]/weights[i],i)for i in weights]
+    result.sort(key=lambda tup:tup[0],reverse=True)
+    return result
